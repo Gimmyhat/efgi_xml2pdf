968a315e296caf2bd29596d961e022a3b6f27e73
nas <nikitchenko.as@gmail.com>
2024-08-22 16:12:09 +0800

    доработки формы вывода и остальное по мелочи

--- app/main_app.py
+++ app/main_app.py
@@ -1,11 +1,10 @@
 import os
 import traceback
-from fastapi import FastAPI, File, UploadFile, HTTPException, Depends, Body
+from fastapi import FastAPI, File, UploadFile, HTTPException, Depends, Body, Request
 from fastapi.responses import HTMLResponse, StreamingResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from app.xml_to_pdf import convert_xml_to_pdf, create_temp_dir, delete_temp_dir
-from xml.etree import ElementTree as ET
 
 app = FastAPI()
 
@@ -30,6 +29,24 @@ def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
     return credentials.username
 
 
+def handle_xml(xml_content: str, project_path: str, xsd_path: str):
+    temp_dir = create_temp_dir(project_path)
+    try:
+        xml_file_path = os.path.join(temp_dir, "temp.xml")
+        with open(xml_file_path, "w", encoding="utf-8") as xml_file:
+            xml_file.write(xml_content)
+
+        pdf_path = convert_xml_to_pdf(xml_file_path, project_path, xsd_path)
+        if not os.path.exists(pdf_path):
+            raise FileNotFoundError(f"Файл PDF не был создан: {pdf_path}")
+
+        return pdf_path
+    finally:
+        pass
+        # if os.path.exists(temp_dir):
+        #     delete_temp_dir(temp_dir)
+
+
 @app.get("/", response_class=HTMLResponse)
 async def upload_page(username: str = Depends(authenticate)):
     try:
@@ -40,103 +57,49 @@ async def upload_page(username: str = Depends(authenticate)):
 
 
 @app.post("/upload/")
-async def upload_file(file: UploadFile = File(...), username: str = Depends(authenticate)):
-    temp_dir = None
+async def upload_file(
+        file: UploadFile = File(None),
+        username: str = Depends(authenticate)
+):
     try:
         project_path = os.path.dirname(os.path.abspath(__file__))
-        temp_dir = create_temp_dir(project_path)
-        xsd_path = os.path.join(project_path, "schemas/schema.xsd")  # Указываем путь к XSD файлу
+        xsd_path = os.path.join(project_path, "schemas/schema.xsd")
 
-        # Сохраняем загруженный файл во временную директорию
-        file_path = os.path.join(temp_dir, file.filename)
+        if not file:
+            raise HTTPException(status_code=400, detail="No file uploaded")
+
+        file_path = os.path.join(create_temp_dir(project_path), file.filename)
         with open(file_path, "wb") as f:
             f.write(file.file.read())
 
-        # Преобразуем XML в PDF
-        pdf_path = convert_xml_to_pdf(file_path, project_path, xsd_path)  # Передаем XSD путь
-
-        # Проверяем, создан ли PDF файл
+        pdf_path = convert_xml_to_pdf(file_path, project_path, xsd_path)
         if not os.path.exists(pdf_path):
-            raise FileNotFoundError(f"Файл PDF не был создан: {pdf_path}")
-
-        # Отправляем PDF файл на клиент
-        return StreamingResponse(open(pdf_path, "rb"), media_type="application/pdf",
-                                 headers={"Content-Disposition": f"inline; filename={file.filename}.pdf"})
-    except Exception as e:
-        # Печать полного трейсбэка ошибки для отладки
-        print(traceback.format_exc())
-        return HTTPException(status_code=500, detail=f"Ошибка при обработке файла: {e}")
-    finally:
-        # Удаляем временную директорию
-        if temp_dir and os.path.exists(temp_dir):
-            delete_temp_dir(temp_dir)
-
-
-# Маршрут для приема XML через POST запрос
-@app.post("/api/upload-xml/")
-async def upload_xml(xml_body: str = Body(..., media_type="application/xml"), username: str = Depends(authenticate)):
-    temp_dir = None
-    try:
-        # Проверяем, валиден ли XML
-        try:
-            root = ET.fromstring(xml_body)
-        except ET.ParseError:
-            raise HTTPException(status_code=400, detail="Invalid XML format")
-
-        project_path = os.path.dirname(os.path.abspath(__file__))
-        temp_dir = create_temp_dir(project_path)
-        xsd_path = os.path.join(project_path, "schemas/schema.xsd")
-
-        # Сохраняем XML во временную директорию с указанием кодировки UTF-8
-        xml_file_path = os.path.join(temp_dir, "temp.xml")
-        with open(xml_file_path, "w", encoding="utf-8") as xml_file:
-            xml_file.write(xml_body)
-
-        # Преобразуем XML в PDF
-        pdf_path = convert_xml_to_pdf(xml_file_path, project_path, xsd_path)
-
-        if not os.path.exists(pdf_path):
-            raise FileNotFoundError(f"Файл PDF не был создан: {pdf_path}")
+            raise FileNotFoundError(f"PDF file was not created: {pdf_path}")
 
         return StreamingResponse(open(pdf_path, "rb"), media_type="application/pdf",
                                  headers={"Content-Disposition": "inline; filename=document.pdf"})
+
     except Exception as e:
         print(traceback.format_exc())
-        raise HTTPException(status_code=500, detail=f"Ошибка при обработке файла: {e}")
-    finally:
-        if temp_dir and os.path.exists(temp_dir):
-            delete_temp_dir(temp_dir)
+        raise HTTPException(status_code=500, detail=f"Error processing file: {e}")
 
 
-# Новый API маршрут для работы по принципу API
-@app.post("/api/upload/")
-async def api_upload(file: UploadFile = File(...), username: str = Depends(authenticate)):
-    temp_dir = None
+@app.post("/upload/xml/")
+async def upload_xml(
+        xml_body: str = Body(..., media_type="application/xml"),
+        username: str = Depends(authenticate)
+):
     try:
         project_path = os.path.dirname(os.path.abspath(__file__))
-        temp_dir = create_temp_dir(project_path)
-        xsd_path = os.path.join(project_path, "schemas/schema.xsd")  # Указываем путь к XSD файлу
+        xsd_path = os.path.join(project_path, "schemas", "schema.xsd")
 
-        # Сохраняем загруженный файл во временную директорию
-        file_path = os.path.join(temp_dir, file.filename)
-        with open(file_path, "wb") as f:
-            f.write(file.file.read())
-
-        # Преобразуем XML в PDF
-        pdf_path = convert_xml_to_pdf(file_path, project_path, xsd_path)  # Передаем XSD путь
-
-        # Проверяем, создан ли PDF файл
+        pdf_path = handle_xml(xml_body, project_path, xsd_path)
         if not os.path.exists(pdf_path):
-            raise FileNotFoundError(f"Файл PDF не был создан: {pdf_path}")
+            raise FileNotFoundError(f"PDF file was not created: {pdf_path}")
 
-        # Отправляем PDF файл обратно как ответ на API запрос
         return StreamingResponse(open(pdf_path, "rb"), media_type="application/pdf",
-                                 headers={"Content-Disposition": f"attachment; filename={file.filename}.pdf"})
+                                 headers={"Content-Disposition": "inline; filename=document.pdf"})
+
     except Exception as e:
-        # Печать полного трейсбэка ошибки для отладки
         print(traceback.format_exc())
-        return HTTPException(status_code=500, detail=f"Ошибка при обработке файла: {e}")
-    finally:
-        # Удаляем временную директорию
-        if temp_dir and os.path.exists(temp_dir):
-            delete_temp_dir(temp_dir)
+        raise HTTPException(status_code=500, detail=f"Error processing XML: {e}")
--- app/xml_to_pdf.py
+++ app/xml_to_pdf.py
@@ -32,50 +32,55 @@ def get_wkhtmltopdf_path():
         raise RuntimeError('Unsupported OS')
 
 
-def extract_annotations_from_xsd(xsd_path):
+def extract_attributes_from_xsd(xsd_path):
     tree = ET.parse(xsd_path)
     root = tree.getroot()
     namespaces = {'xs': 'http://www.w3.org/2001/XMLSchema'}
 
-    annotations = {}
+    attributes_data = {}
 
-    # Проход по всем complexType и element
+    # Проход по всем element и complexType в XSD
     for element in root.findall(".//xs:element", namespaces):
-        name = element.attrib.get('name')
-        doc = element.find('.//xs:documentation', namespaces)
-        if name and doc is not None:
-            annotations[name] = doc.text
+        element_name = element.attrib.get('name')
 
-    for complex_type in root.findall(".//xs:complexType", namespaces):
-        name = complex_type.attrib.get('name')
-        doc = complex_type.find('.//xs:documentation', namespaces)
-        if name and doc is not None:
-            annotations[name] = doc.text
+        # Найдем атрибуты для каждого элемента
+        attributes = element.findall('.//xs:attribute', namespaces)
+        element_attributes = {}
+        for attribute in attributes:
+            attr_name = attribute.attrib.get('name')
+            attr_type = attribute.attrib.get('type')
+            element_attributes[attr_name] = attr_type
 
-    return annotations
+        if element_name and element_attributes:
+            attributes_data[element_name] = element_attributes
+
+    return attributes_data
 
 
-def parse_element(element, xsd_annotations):
+def parse_element(element, xsd_attributes):
     """
     Рекурсивная функция для обработки XML-элементов и создания структуры данных для шаблона.
     """
     parsed_data = {
-        "name": xsd_annotations.get(element.tag, element.tag),
+        "name": element.tag,
         "value": element.text.strip() if element.text and element.text.strip() else None,
         "attributes": [],
         "children": []
     }
 
-    # Обработка атрибутов элемента
+    # Обработка атрибутов элемента на основе информации из XSD
+    element_attributes = xsd_attributes.get(element.tag, {})
     for attr, value in element.attrib.items():
+        attr_type = element_attributes.get(attr, 'unknown')
         parsed_data["attributes"].append({
-            "name": xsd_annotations.get(attr, attr),
-            "value": value
+            "name": attr,
+            "value": value,
+            "type": attr_type  # Добавляем тип атрибута
         })
 
     # Обработка дочерних элементов
     for child in element:
-        child_data = parse_element(child, xsd_annotations)
+        child_data = parse_element(child, xsd_attributes)
         if child_data['value'] or child_data['children']:
             parsed_data["children"].append(child_data)
 
@@ -99,7 +104,7 @@ def convert_xml_to_pdf(xml_path: str, project_path: str, xsd_path: str):
         pdf_path = os.path.join(temp_dir, "output.pdf")
 
         # Получаем аннотации из XSD схемы
-        xsd_annotations = extract_annotations_from_xsd(xsd_path)
+        xsd_annotations = extract_attributes_from_xsd(xsd_path)
 
         # Определение кодировки XML-файла (для парсинга, а не для записи HTML)
         with open(xml_path, "rb") as file:
@@ -110,15 +115,26 @@ def convert_xml_to_pdf(xml_path: str, project_path: str, xsd_path: str):
         # Парсим XML и подготавливаем данные для шаблона
         tree = ET.parse(xml_path)
         root = tree.getroot()
-        parsed_data = parse_element(root, xsd_annotations)
+        data = parse_element(root, xsd_annotations)
 
-        # Подготовка данных для шаблона
+        # Преобразование данных из словаря в формат, подходящий для шаблона
         context = {
-            "root_element": parsed_data
+            "name": find_value(data, 'FullName'),
+            "last_name": find_value(data, 'LastName'),  # Добавляем фамилию
+            "first_name": find_value(data, 'FirstName'),  # Добавляем имя
+            "middle_name": find_value(data, 'MiddleName'),  # Добавляем отчество
+            "inn": find_value(data, 'INN'),
+            "snils": find_value(data, 'RepresentativeSNILS'),
+            "tel": find_value(data, 'Phone'),
+            "email": find_value(data, 'Email'),
+            "date": find_value(data, 'RequestDate'),
+            "inv": find_value(data, 'UniqueID'),
+            "coords": extract_coordinates(data),  # Функция для извлечения координат
+            "cad": find_value(data, 'CadastralNumber')
         }
 
         # Рендерим шаблон с данными
-        html_content = render_template("template.html", context, project_path)
+        html_content = render_template("template2.html", context, project_path)
 
         # Сохраняем сгенерированный HTML во временный файл с кодировкой UTF-8
         with open(html_path, "w", encoding="utf-8") as file:  # Принудительное использование UTF-8
@@ -136,3 +152,44 @@ def convert_xml_to_pdf(xml_path: str, project_path: str, xsd_path: str):
     except Exception as e:
         print(f"Ошибка при конвертации XML в PDF: {e}")
         raise
+
+
+def find_value(data, target_name):
+    """
+    Рекурсивно ищет значение элемента по имени в словаре.
+
+    :param data: Словарь, представляющий дерево элементов.
+    :param target_name: Имя элемента, значение которого нужно найти.
+    :return: Значение элемента с именем target_name, или None, если не найдено.
+    """
+    # Если текущий элемент содержит нужное имя, возвращаем его значение
+    if data['name'] == target_name:
+        return data['value']
+
+    # Если элемент имеет детей, рекурсивно ищем среди них
+    if 'children' in data:
+        for child in data['children']:
+            result = find_value(child, target_name)
+            if result is not None:
+                return result
+
+    # Если имя не найдено в текущем элементе или среди детей, возвращаем None
+    return None
+
+
+def extract_coordinates(data):
+    coords = []
+
+    # Поиск координат в словаре
+    def recursive_search(d):
+        if d['name'] == 'Point':
+            lat = find_value(d, 'Latitude')
+            lon = find_value(d, 'Longitude')
+            if lat and lon:
+                coords.append(f"{lat}, {lon}")
+        if 'children' in d:
+            for child in d['children']:
+                recursive_search(child)
+
+    recursive_search(data)
+    return coords
